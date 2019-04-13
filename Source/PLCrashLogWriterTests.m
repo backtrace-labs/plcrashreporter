e5c1cdc59f985d51652cb5e546f8b56916f3935c
Marcin Karmelita <marcin@apptailors.co>
2019-04-13 12:48:39 +0200

    Update deployment targets: iOS 9.3, mac 10.8. Remove no longer supported architectures: i386, ppc
    Update project settings
    
    Update valid architectures for macOS
    
    Remove Lipo binary build phase
    
    Remove AppKit dependency from iOS target
    
    Update iOS target and remove UIKit dependency
    
    Update crash_report.proto
    
    Disable mach_exceptions on tvOS
    
    Update podspec

--- Source/PLCrashLogWriterTests.m
+++ Source/PLCrashLogWriterTests.m
@@ -181,9 +181,12 @@
     /* Parent process; fetching the process info is expected to fail on iOS 9+ due to new sandbox constraints */
     PLCrashProcessInfo *parentProcessInfo = [[[PLCrashProcessInfo alloc] initWithProcessID: getppid()] autorelease];
 
-    if (PLCrashReportHostOperatingSystem == PLCrashReportOperatingSystemiPhoneOS && PLCrashHostInfo.currentHostInfo.darwinVersion.major >= PLCRASH_HOST_IOS_DARWIN_MAJOR_VERSION_9) {
-        STAssertNil(parentProcessInfo, @"Fetching parent process info unexpectedly succeeded on iOS");
-        STAssertNULL(procInfo->parent_process_name, @"Fetching parent process info unexpectedly succeeded on iOS");
+    if (PLCrashReportHostOperatingSystem == PLCrashReportOperatingSystemAppleTVOS ||
+        (PLCrashReportHostOperatingSystem == PLCrashReportOperatingSystemiPhoneOS &&
+         PLCrashHostInfo.currentHostInfo.darwinVersion.major >= PLCRASH_HOST_IOS_DARWIN_MAJOR_VERSION_9))
+    {
+        STAssertNil(parentProcessInfo, @"Fetching parent process info unexpectedly succeeded on iOS-derived OS");
+        STAssertNULL(procInfo->parent_process_name, @"Fetching parent process info unexpectedly succeeded on iOS-derived OS");
         
     } else {
         STAssertNotNil(parentProcessInfo, @"Could not retrieve parent process info");

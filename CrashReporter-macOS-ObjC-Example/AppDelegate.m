fb3cd6378f5f3aa7e1779482f15f17ee24e029dd
Marcin Karmelita <marcin@apptailors.co>
2020-07-21 01:09:43 +0200

    Re-create crash

--- CrashReporter-macOS-ObjC-Example/AppDelegate.m
+++ CrashReporter-macOS-ObjC-Example/AppDelegate.m
@@ -1,6 +1,8 @@
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface AppDelegate ()<NSMenuDelegate, NSMenuItemValidation>
+@property (weak) IBOutlet NSMenu *menu;
+@property (weak) IBOutlet NSMenuItem *menuItem;
 
 @end
 
@@ -8,6 +10,9 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    self.menu.delegate = self;
+    self.menuItem.target = self;
+    self.menuItem.action = @selector(foo);
 }
 
 
@@ -15,5 +20,19 @@
     // Insert code here to tear down your application
 }
 
+- (void) foo {
+    
+}
+
+-(NSString *) concatString:(NSString*) s1 toString:(NSString *) s2 {
+    return [s1 stringByAppendingString:s2];
+}
+
+- (BOOL) validateMenuItem:(NSMenuItem *)item {
+    BOOL result = NO;
+    NSLog(@"%@", [self concatString:@"bob" toString:nil]);
+    
+    return result;
+}
 
 @end
